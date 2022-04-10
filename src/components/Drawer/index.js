82aad2cd0c18afc139934140d0ccca7663ef8dac
Baltazar <balti.kpo@gmail.com>
2022-04-10 13:19:27 -0300

    Added drawer to mobile version

---
+++ src/components/Drawer/index.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { CgMenu } from "react-icons/cg";
+import { Rooms } from "../Rooms";
+
+export function Drawer() {
+  const [showDrawer, setShowDrawer] = useState(false);
+
+  function toggleDrawer() {
+    setShowDrawer((prevState) => !prevState);
+  }
+
+  return (
+    <div className="h-full flex items-center sm:hidden ">
+      <button onClick={toggleDrawer}>
+        <CgMenu size={32} />
+      </button>
+      <nav
+        className={`absolute transition-transform duration-500 left-0 top-0 right-0 bottom-0 bg-zinc-900 z-20 ${
+          showDrawer ? "flex translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <Rooms props={{ showDrawer, toggleDrawer }} />
+      </nav>
+    </div>
+  );
+}
