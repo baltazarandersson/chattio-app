a43f792f5990f96bf3e9be42e5de5c85c5d3f0e0
Baltazar <balti.kpo@gmail.com>
2022-04-11 20:18:06 -0300

    Fix to drawer

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -15,7 +15,7 @@ export function Drawer() {
         <CgMenu size={32} />
       </button>
       <nav
-        className={`absolute transition-transform duration-500 left-0 top-0 right-0 bottom-0 bg-zinc-50 dark:bg-zinc-900 z-20 ${
+        className={`absolute transition-transform duration-500 left-0 right-0 top-0 h-screen bg-zinc-50 dark:bg-zinc-900 z-20 ${
           showDrawer ? "flex translate-x-0" : "-translate-x-full"
         }`}
       >
